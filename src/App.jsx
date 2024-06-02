0e2c63ee56760d6fa1f27bd4e80a77954490af86
Dashrath Patel <dashrathpatel0810@gmail.com>
2024-06-02 15:10:28 +0530

    Added the functionality of searchParams and location Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
-import Header from "./components/Header"
-import Blogs from "./components/Blogs"
+/* eslint-disable no-unused-vars */
+import Home from "./pages/Home";
+import CategoryPage from "./pages/CategoryPage";
+import BlogPage from "./pages/BlogPage";
+import TagPage from "./pages/TagPage";
 import { useContext, useEffect } from "react"
-import Pagination from "./components/Pagination"
 import { AppContext } from "./context/AppContext"
 import { Route,Routes } from "react-router-dom"
+import { useSearchParams } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
+
+function App() {
 const [searchParams,setSearchParams]=useSearchParams();
 const location =useLocation()
-function App() {
   const {fetchBlogPosts} = useContext(AppContext)
   useEffect(() => 
     {
-      fetchBlogPosts()
-    },[]);
+     const page =searchParams.get("page"?? 1);
+     if(location.pathname.includes("tags")){
+      //iska matlab tag wala page show karna hai
+      const tag=location.pathname.split("/").at(-1).replaceAll("-"," ");
+      fetchBlogPosts(Number(page),tag)
+;     }
+else if(location.pathname.includes("categories")){
+  const category=location.pathname.split("/").at(-1).replaceAll("-"," ");
+  fetchBlogPosts(Number(page),null,category)
+}
+else{
+  fetchBlogPosts(Number(page));
+}
+    },[location.pathname, location.search]);
       return (
-     <Route >
+     <Routes >
       <Route path="/" element={<Home/>}/>
       <Route path="/blog/:blogId" element={<BlogPage/>}/>
       <Route path="/tags/:tag" element={<TagPage/>}/>
       <Route path="/categories/:category" element={<CategoryPage/>}/>
-     </Route>
+     </Routes>
   )
 }
 
 export default App
-//ok
